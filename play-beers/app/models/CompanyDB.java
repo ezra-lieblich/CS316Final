c5aad6f0893b3da1df44abe1943227b64f5fe0e8
Ezra <eml36@duke.edu>
2016-12-13 13:53:16 -0500

    stared working on CompanyDB model

---
+++ play-beers/app/models/CompanyDB.java
@@ -0,0 +1,14 @@
+package models;
+
+import play.db.*;
+import java.sql.*;
+import javax.sql.*;
+import java.util.ArrayList;
+
+public class CompanyDB {
+    public static class CompanyInfo {
+        public CompanyInfo(String key) {
+
+        }
+    }
+}
