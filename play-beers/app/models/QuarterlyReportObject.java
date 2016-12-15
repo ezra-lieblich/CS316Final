8e095b8aaba762f743c619d582771bbbf3e58ee2
Diane Hadley <dfh12@duke.edu>
2016-12-15 04:19:32 -0500

    Write query for quarterly reports

---
+++ play-beers/app/models/QuarterlyReportObject.java
@@ -0,0 +1,18 @@
+package models;
+
+public class QuarterlyReportObject {
+	
+	public String name;
+	public String date;
+	public String path;
+	
+	public QuarterlyReportObject(String name, String date, String path){
+		this.name = name;
+		this.date = date;
+		this.path = path;
+	}
+	
+	
+	
+
+}
